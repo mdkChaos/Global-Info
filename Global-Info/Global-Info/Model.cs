e26006e34734fa21852ed5cb666c9b0b69a0f33d
mdkChaos <mdkchaos1@gmail.com>
2017-09-09 21:38:45 +0200

    Relise get name city

--- Global-Info/Global-Info/Model.cs
+++ Global-Info/Global-Info/Model.cs
@@ -15,12 +15,30 @@ namespace Global_Info
             var web = new HtmlWeb();
             var page = web.Load(webPage);
             HtmlNode weather = page.DocumentNode.SelectSingleNode(idTag);
+            Dictionary<string, string> citys = new Dictionary<string, string>();
+            var nodes = page.DocumentNode.SelectNodes("//*[@class='m13']");
+            foreach (var node in nodes)
+            {
+                citys.Add(node.InnerText, node.Attributes[0].Value);
+            }
             return weather.InnerText;
-
-            //var page = web.Load("http://meteo.gov.ua/ua/33791");
             //var page = web.Load("https://finance.yahoo.com/currencies");
             //HtmlNode eurusd = page.DocumentNode.SelectSingleNode("//*[@id='yfin-list']/div[2]/div/table/tbody/tr[3]/td[3]");
-            //HtmlNode weather = page.DocumentNode.SelectSingleNode("//*[@id='curWeatherT']");
+        }
+        public SortedDictionary<string, string> GetURLAndNameCitys(string webPage, string idTag)
+        {
+            SortedDictionary<string, string> citys = new SortedDictionary<string, string>();
+            var web = new HtmlWeb();
+            var page = web.Load(webPage);
+            var nodes = page.DocumentNode.SelectNodes(idTag);
+            if (nodes != null)
+            {
+                foreach (var node in nodes)
+                {
+                    citys.Add(node.InnerText, node.Attributes[0].Value);
+                }
+            }
+            return citys;
         }
     }
 }
--- Global-Info/Global-Info/Presenter.cs
+++ Global-Info/Global-Info/Presenter.cs
@@ -10,23 +10,50 @@ namespace Global_Info
     {
         Model model = null;
         MainWindow mainWindow = null;
+        SortedDictionary<string, string> city = new SortedDictionary<string, string>();
 
         public Presenter(MainWindow mainWindow)
         {
-            this.model = new Model();
+            model = new Model();
             this.mainWindow = mainWindow;
+            city = model.GetURLAndNameCitys("http://meteo.gov.ua", "//div[@class='selec1']/a[@class='m13']");
+            if (city!=null)
+            {
+                foreach (var item in city)
+                {
+                    this.mainWindow.ComboBox1.Items.Add(item.Key);
+                }
+            }
+            else
+            {
+                this.mainWindow.ComboBox1.Items.Add("City didn't found");
+            }
+            this.mainWindow.ComboBox1.SelectedIndex = 0;
             this.mainWindow.myEvent += new EventHandler(mainWindow_myEvent);
         }
 
-        void mainWindow_myEvent(object sender, EventArgs e)
+        private void mainWindow_myEvent(object sender, EventArgs e)
         {
-            string variable = model.GetData("http://meteo.gov.ua/ua/33791", "//*[@id='curWeatherT']");
-            if (variable != null)
+            string siteUrl = city[mainWindow.ComboBox1.Text];
+            var temperature = model.GetData(siteUrl, "//*[@id='curWeatherT']");
+            if (temperature != null)
             {
-                this.mainWindow.Label2.Content = variable;
+                mainWindow.Label2.Content = temperature + " °C";
             }
             else
-                this.mainWindow.Label2.Content = "No info";
+            {
+                mainWindow.Label2.Content = "No info";
+            }
+
+            //var city = model.GetData("http://meteo.gov.ua/ua/33345", "//*[@class='hdr_fr_bl1_sity']");
+            //if (city != null)
+            //{
+            //    mainWindow.Label3.Content = city;
+            //}
+            //else
+            //{
+            //    mainWindow.Label1.Content = "No info";
+            //}
         }
     }
 }
